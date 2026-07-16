5a9537d933198d7058c62f444035ed6fdf66dffb
mina <k.minami.kanade@gmail.com>
2026-07-17 00:36:57 +0900

    no message

---
+++ 1-cmd/2-出荷/1-1-移动Exce-第三个空格后面的不要 copy/MoveExcel.ps1
@@ -0,0 +1,113 @@
+﻿$SourcePaths = @(
+    'C:\c_wk\10_会社\PDF-相关\Test',
+    'C:\c_wk\10_会社\PDF-相关\Test2'
+)
+
+$TargetPath = 'C:\c_wk\10_会社\PDF-相关\完成'
+
+
+$TargetNumbers = @(
+    '1231T222',
+    '1231T4422'
+)
+
+
+if (!(Test-Path $TargetPath)) {
+
+    New-Item `
+        -ItemType Directory `
+        -Path $TargetPath `
+        -Force |
+        Out-Null
+}
+
+
+$count = 0
+
+
+
+foreach ($path in $SourcePaths) {
+
+
+    Get-ChildItem $path -File -Recurse |
+    Where-Object {
+        $_.Extension -in ".xls", ".xlsx"
+    } |
+    ForEach-Object {
+
+
+        foreach ($num in $TargetNumbers) {
+
+
+            if ($_.Name -like "*$num*") {
+
+
+                $name = $_.BaseName
+
+
+                # =========================
+                # 第三个空格以后删除
+                # 半角空格、全角空格都处理
+                # =========================
+
+                $parts = $name -split '[\s　]+'
+
+
+                if ($parts.Count -ge 3) {
+
+                    $newBaseName = (
+                        $parts[0],
+                        $parts[1],
+                        $parts[2]
+                    ) -join ' '
+
+                }
+                else {
+
+                    $newBaseName = $name
+
+                }
+
+
+
+                $newName = (
+                    $newBaseName
+                    +
+                    $_.Extension
+                )
+
+
+                Write-Host "COPY => $newName"
+
+
+
+                Copy-Item `
+                    -Path $_.FullName `
+                    -Destination (
+                        Join-Path `
+                        $TargetPath `
+                        $newName
+                    ) `
+                    -Force
+
+
+
+                $count++
+
+                break
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+Write-Host ""
+Write-Host "TOTAL : $count"
+
+
+Read-Host "Press Enter"
